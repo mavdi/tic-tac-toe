01c360b1f4afdfbf2230feb03fd12ea7c3d156ca
Mehdi Avdi <mehdi.avdi@cornucopic.com>
2016-08-25 11:36:31 +0200

    Simple routing

--- frontend/app/modules/app.config.js
+++ frontend/app/modules/app.config.js
@@ -5,6 +5,9 @@
     angular.module('ticTacToe').config(function ($routeProvider) {
 
         $routeProvider.when('/', {
+            templateUrl: 'modules/home/home.tpl.html'
+        });
+        $routeProvider.when('/play', {
             templateUrl: 'modules/play/play.tpl.html'
         });
         $routeProvider.otherwise({redirectTo: '/'});
