4fcb0648e03a3ec612f4b5548623466938759f8d
Mehdi Avdi <mehdi.avdi@cornucopic.com>
2016-08-26 12:01:13 +0200

    Move messages to html

--- frontend/app/modules/game/game.controller.js
+++ frontend/app/modules/game/game.controller.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    function playController($location, $routeParams, $scope) {
+    function playController($location, $routeParams, $scope, $timeout) {
         var socket = io.connect('http://localhost:3000');
 
         var id;
@@ -16,13 +16,17 @@
             if ($routeParams.id) {
                 socket.emit('room:join', $routeParams.id);
                 socket.on('room:join:response', function () {
-                    ctrl.state = 'The game is started!';
+                    ctrl.state = 'opponent-join';
                     $scope.$apply();
+                    $timeout(function () {
+                        ctrl.state = 'opponent-move';
+                    }, 3000);
+
                 });
             } else {
                 socket.emit('room:create');
                 socket.on('room:create:response', function (data) {
-                    ctrl.state = 'Waiting for opponent...';
+                    ctrl.state = 'waiting';
                     $location.search({id: data.id});
                     $scope.$apply();
                 });
@@ -35,12 +39,15 @@
         };
 
         socket.on('room:opponent:join', function () {
-            ctrl.state = 'The game is started!';
+            ctrl.state = 'opponent-join';
             $scope.$apply();
+            $timeout(function () {
+                ctrl.state = 'your-move';
+            }, 3000);
         });
 
         socket.on('room:opponent:leave', function () {
-            ctrl.state = 'Opponent is disconnected!';
+            ctrl.state = 'opponent-leave';
             $scope.$apply();
         });
 
@@ -51,5 +58,5 @@
         init();
     }
 
-    angular.module('ticTacToe').controller('Game', ['$location', '$routeParams', '$scope', playController]);
+    angular.module('ticTacToe').controller('Game', ['$location', '$routeParams', '$scope', '$timeout', playController]);
 })();
